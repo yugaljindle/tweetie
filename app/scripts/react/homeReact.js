ad315425feb91d8a90c5a87d335c24de198f45b0
Yugal Jindle <yugal.jindle@proptiger.com>
2013-12-28 19:19:12 +0530

    Asthetic changes.

--- app/scripts/react/homeReact.js
+++ app/scripts/react/homeReact.js
@@ -6,7 +6,7 @@
 var ListHashtags = React.createClass({
   render: function() {
     var getList = function(tag) {
-      return <li className="text-success">{tag}</li>;
+      return <li className="text-info">{tag}</li>;
     };
     return (
         <ul className="nav nav-list taglist">
@@ -20,39 +20,42 @@ var ListHashtags = React.createClass({
  * Form to add hashtags
  * */
 var AddHashtag = React.createClass({
-  getInitialState: function() {
-    return {tags: [], currTag: ''};
-  },
-  onChange: function(e) {
-    var v = e.target.value.trim();
-    this.setState({currTag: v});
-  },
-  onSubmit: function(e) {
-    e.preventDefault();
-    if(!this.state.currTag) {
-        return;
+    getInitialState: function() {
+        return {tags: [], currTag: ''};
+    },
+    onChange: function(e) {
+        var v = e.target.value.trim();
+        this.setState({currTag: v});
+    },
+    onSubmit: function(e) {
+        e.preventDefault();
+        if(!this.state.currTag) {
+            return;
+        }
+        var tag = '#' + this.state.currTag;
+            updatedTags = this.state.tags.concat([tag]);
+        this.setState({tags: updatedTags, currTag: ''});
+        // Set angular scope
+        window.scope.tags.push(tag);
+        window.scope.$apply();
+    },
+    componentDidMount: function() {
+        $('.addNewTag').focus();
+    },
+    render: function() {
+        return (
+            <div className="follow-hashtag">
+                <form onSubmit={this.onSubmit}>
+                    <div className="input-prepend input-append">
+                        <span className="add-on">#</span>
+                        <input className="span9 addNewTag" type="text" placeholder="hashtag" onChange={this.onChange} value={this.state.currTag} />
+                        <button className="btn">Follow</button>
+                    </div>
+                </form>
+                <ListHashtags tags={this.state.tags} />
+            </div>
+        );
     }
-    var tag = '#' + this.state.currTag;
-        updatedTags = this.state.tags.concat([tag]);
-    this.setState({tags: updatedTags, currTag: ''});
-    // Set angular scope
-    window.scope.tags.push(tag);
-    window.scope.$apply();
-  },
-  render: function() {
-    return (
-        <div className="follow-hashtag">
-            <form onSubmit={this.onSubmit}>
-                <div className="input-prepend input-append">
-                    <span className="add-on">#</span>
-                    <input className="span9" type="text" placeholder="hashtag" onChange={this.onChange} value={this.state.currTag} />
-                    <button className="btn">Follow</button>
-                </div>
-            </form>
-            <ListHashtags tags={this.state.tags} />
-        </div>
-    );
-  }
 });
 
 // Function to attach <AddHashtag />
@@ -68,7 +71,9 @@ var TweetieShoutbox = React.createClass({
   render: function() {
     return (
         <div className="tweet-box">
-            {this.props.tag}
+            <div className="heading">
+                <span className="headingText">{this.props.tag}</span> 
+            </div>
         </div>
     );
   }
@@ -84,17 +89,27 @@ var TweetieShoutboxList = React.createClass({
     updateTags: function() {
         this.setState({tags: window.displayTags});
     },
+    showList: function() {
+        var createShoutBox = function(tag) {
+            return <TweetieShoutbox tag={tag} />;
+        };
+
+        if(this.state.tags.length !== 0) {
+            return this.state.tags.map(createShoutBox);
+        } else {
+            return (<div className="empty-banner">
+                {"Enter new #Hashtag !"}
+            </div>);
+        }
+    },
     componentDidMount: function() {
         this.interval = setInterval(this.updateTags, 500);
     },
     render: function() {
-        var createShoutBox = function(tag) {
-            return <TweetieShoutbox tag={tag} />;
-        };
-        
+
         return (
             <div className="shout-list">
-                {this.state.tags.map(createShoutBox)}
+                {this.showList()}
             </div>
         );
     }
@@ -102,5 +117,5 @@ var TweetieShoutboxList = React.createClass({
 
 // Function to add <TweetieShoutboxList />
 function addTweetieShoutboxList() {
-    React.renderComponent(<TweetieShoutboxList />, document.getElementById('shouthere'));
+    React.renderComponent(<TweetieShoutboxList />, document.getElementById('shout-here'));
 }
